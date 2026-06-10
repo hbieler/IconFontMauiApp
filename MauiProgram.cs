76b0f75950e1349f8b002cdb393e8dc1c89187dd
hbieler <hbieler@hbieler.de>
2026-06-10 12:08:47 +0200

    Add files via upload
    initial commit

---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using IconFont.Maui.FluentIcons;
+using Microsoft.Extensions.Logging;
+using IconFont.Maui.FluentIcons;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace IconFontMauiApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+
+                .UseFluentIcons()
+
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("FluentIconsRegular.ttf", "FluentIconsRegular");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
